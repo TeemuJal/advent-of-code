da74f65ba6e4f0e90d4f25a07e158f01ad18ecfb
TeemuJal <temesklk@gmail.com>
2024-10-26 23:42:02 +0300

    day5 part two proper fast solution

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -27,23 +27,60 @@ fn main() {
     let part_one_min_location_number = part_one_location_numbers.iter().min().unwrap();
     println!("Part one lowest location number: {part_one_min_location_number}");
 
-    let mut part_two_min_location_number = i64::MAX;
     let seed_numbers_range_pairs = seed_numbers.chunks(2);
-    for pair in seed_numbers_range_pairs {
-        println!("pair {:?}", pair);
-        let start = pair[0];
-        let len = pair[1];
-        let seeds: Vec<_> = (start..(start + len)).collect();
-        let location_numbers: Vec<i64> = seeds
-            .iter()
-            .map(|seed_number| seed_number_to_location_number(*seed_number, &map_vecs))
-            .collect();
-        let min_location_number = location_numbers.iter().min().unwrap();
-        if *min_location_number < part_two_min_location_number {
-            part_two_min_location_number = *min_location_number;
-        }
-    }
+    let part_two_min_location_number = seed_numbers_range_pairs
+        .map(|pair| {
+            let start = pair[0];
+            let end = start + pair[1] - 1;
+            let seed_range = (start, end);
+            seed_range_to_location_range(seed_range, &map_vecs)
+        })
+        .flatten()
+        .map(|range| range.0)
+        .min()
+        .unwrap();
     println!("Part two lowest location number: {part_two_min_location_number}");
+}
+
+fn seed_range_to_location_range(
+    seed_range: (i64, i64),
+    map_vecs: &Vec<Vec<MapEntry>>,
+) -> Vec<(i64, i64)> {
+    let mut ranges_to_process = vec![seed_range];
+    let mut map_result_ranges = vec![];
+    for map in map_vecs {
+        'range_loop: while ranges_to_process.len() > 0 {
+            let (range_start, range_end) = ranges_to_process.pop().unwrap();
+            for entry in map {
+                let (destination_range_start, source_range_start, range_len) = *entry;
+                let source_range_end = source_range_start + range_len - 1;
+                let offset = destination_range_start - source_range_start;
+                if range_start >= source_range_start && range_start <= source_range_end {
+                    let result_range_start = range_start + offset;
+                    let result_range_end = if range_end <= source_range_end {
+                        range_end
+                    } else {
+                        ranges_to_process.push((source_range_end + 1, range_end));
+                        source_range_end
+                    };
+                    let result_range_end = result_range_end + offset;
+                    map_result_ranges.push((result_range_start, result_range_end));
+                    continue 'range_loop;
+                }
+                if range_end <= source_range_end && range_end >= source_range_start {
+                    let result_range_end = range_end + offset;
+                    let result_range_start = source_range_start + offset;
+                    map_result_ranges.push((result_range_start, result_range_end));
+                    ranges_to_process.push((range_start, source_range_start - 1));
+                    continue 'range_loop;
+                }
+            }
+            map_result_ranges.push((range_start, range_end));
+        }
+        ranges_to_process = map_result_ranges.clone();
+        map_result_ranges = vec![];
+    }
+    return ranges_to_process;
 }
 
 fn seed_number_to_location_number(seed_number: i64, map_vecs: &Vec<Vec<MapEntry>>) -> i64 {
