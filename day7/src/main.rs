5000e98ead81fbf621d50950418cba070ee9390a
TeemuJal <temesklk@gmail.com>
2024-11-09 22:47:49 +0200

    day7 part two solution

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,10 +1,10 @@
 use std::{cmp::Ordering, collections::HashSet, fs::read_to_string};
 
+const JOKER: char = 'J';
 const CARDS: [char; 13] = [
-    '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
+    JOKER, '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A',
 ];
 
-#[derive(Debug)]
 struct Hand {
     cards: [char; 5],
     hand_type: u8,
@@ -15,8 +15,12 @@ fn main() {
     let mut hand_bid_pairs: Vec<(Hand, usize)> =
         input.lines().map(parse_line_to_hand_bid_pair).collect();
     hand_bid_pairs.sort_by(compare_hand_bid_pair);
-    let part_one_result: usize = hand_bid_pairs.iter().enumerate().map(|(idx, (_, bid))| bid * (idx + 1)).sum();
-    println!("Part one result is {part_one_result}");
+    let total_winnings: usize = hand_bid_pairs
+        .iter()
+        .enumerate()
+        .map(|(idx, (_, bid))| bid * (idx + 1))
+        .sum();
+    println!("Part two result is {total_winnings}");
 }
 
 fn compare_hand_bid_pair((hand_1, _): &(Hand, usize), (hand_2, _): &(Hand, usize)) -> Ordering {
@@ -40,7 +44,7 @@ fn compare_hand_bid_pair((hand_1, _): &(Hand, usize), (hand_2, _): &(Hand, usize
             return card_1_strength.cmp(&card_2_strength);
         }
     }
-    Ordering::Equal
+    return Ordering::Equal;
 }
 
 fn parse_line_to_hand_bid_pair(str: &str) -> (Hand, usize) {
@@ -62,52 +66,56 @@ fn parse_line_to_hand_bid_pair(str: &str) -> (Hand, usize) {
 
 fn get_hand_type(cards: [char; 5]) -> u8 {
     let [first, second, third, fourth, fifth] = cards;
-    let unique_cards_len = HashSet::from(cards).len();
-    // Five of a kind
-    if cards.iter().all(|card| *card == first) {
-        return 6;
-    }
-    // Four of a kind
-    let four_first_chars = cards.iter().filter(|card| **card == first).count() == 4;
-    let four_second_chars = cards.iter().filter(|card| **card == second).count() == 4;
-    if four_first_chars || four_second_chars {
-        return 5;
-    }
-    let three_first_chars = cards.iter().filter(|card| **card == first).count() == 3;
-    let three_second_chars = cards.iter().filter(|card| **card == second).count() == 3;
-    let three_third_chars = cards.iter().filter(|card| **card == third).count() == 3;
-    if three_first_chars || three_second_chars || three_third_chars {
-        // Full house
-        if unique_cards_len == 2 {
-            return 4;
-        }
-        // Three of a kind
-        return 3;
-    }
-    let two_first_chars = cards.iter().filter(|card| **card == first).count() == 2;
-    let two_second_chars = cards.iter().filter(|card| **card == second).count() == 2;
-    let two_third_chars = cards.iter().filter(|card| **card == third).count() == 2;
-    let two_fourth_chars = cards.iter().filter(|card| **card == fourth).count() == 2;
-    let two_fifth_chars = cards.iter().filter(|card| **card == fifth).count() == 2;
+    let unique_cards_count = HashSet::from(cards).len();
+    let joker_count = cards.iter().filter(|card| **card == JOKER).count();
+
+    let first_card_count = cards.iter().filter(|card| **card == first).count();
+    let second_card_count = cards.iter().filter(|card| **card == second).count();
+    let third_card_count = cards.iter().filter(|card| **card == third).count();
+    let fourth_card_count = cards.iter().filter(|card| **card == fourth).count();
+    let fifth_card_count = cards.iter().filter(|card| **card == fifth).count();
+
+    let three_same_chars = first_card_count == 3 || second_card_count == 3 || third_card_count == 3;
 
     let pairs_count = [
-        two_first_chars,
-        two_second_chars,
-        two_third_chars,
-        two_fourth_chars,
-        two_fifth_chars,
+        first_card_count == 2,
+        second_card_count == 2,
+        third_card_count == 2,
+        fourth_card_count == 2,
+        fifth_card_count == 2,
     ]
     .iter()
     .filter(|boolean| **boolean)
     .count()
         / 2;
 
+    // Five of a kind
+    if unique_cards_count == 1 || (unique_cards_count == 2 && joker_count > 0) {
+        return 6;
+    }
+    // Four of a kind
+    if first_card_count == 4
+        || second_card_count == 4
+        || joker_count == 3
+        || (pairs_count == 2 && joker_count == 2)
+        || three_same_chars && joker_count == 1
+    {
+        return 5;
+    }
+    // Full house
+    if unique_cards_count == 3 && joker_count > 0 || unique_cards_count == 2 {
+        return 4;
+    }
+    // Three of a kind
+    if three_same_chars || joker_count == 2 || (pairs_count == 1 && joker_count == 1) {
+        return 3;
+    }
     // Two pairs
     if pairs_count == 2 {
         return 2;
     }
     // One pair
-    if pairs_count == 1 {
+    if pairs_count == 1 || joker_count == 1 {
         return 1;
     }
     return 0;
