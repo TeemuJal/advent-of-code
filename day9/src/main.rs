917c761cfb8f5bc7a14c8a0c17f683936567e6b7
TeemuJal <temesklk@gmail.com>
2024-11-15 17:15:50 +0200

    day9 part two solution

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,11 +2,13 @@ use std::fs::read_to_string;
 
 fn main() {
     let input = read_to_string("input2.txt").unwrap();
-    let sum_of_predictions: isize = input.lines().map(parse_line).map(get_prediction).sum();
-    println!("Sum of predictions is {sum_of_predictions}");
+    let sum_of_future_predictions: isize = input.lines().map(parse_line).map(get_future_prediction).sum();
+    let sum_of_past_predictions: isize = input.lines().map(parse_line).map(get_past_prediction).sum();
+    println!("Sum of future predictions is {sum_of_future_predictions}");
+    println!("Sum of past predictions is {sum_of_past_predictions}");
 }
 
-fn get_prediction(values: Vec<isize>) -> isize {
+fn get_future_prediction(values: Vec<isize>) -> isize {
     if values.iter().all(|val| *val == 0) {
         return 0;
     }
@@ -14,7 +16,19 @@ fn get_prediction(values: Vec<isize>) -> isize {
     for (idx, val) in values.iter().enumerate().take(values.len() - 1) {
         differences.push(values[idx + 1] - val);
     }
-    let prediction = *values.last().unwrap() + get_prediction(differences);
+    let prediction = *values.last().unwrap() + get_future_prediction(differences);
+    return prediction;
+}
+
+fn get_past_prediction(values: Vec<isize>) -> isize {
+    if values.iter().all(|val| *val == 0) {
+        return 0;
+    }
+    let mut differences: Vec<isize> = Vec::new();
+    for (idx, val) in values.iter().enumerate().take(values.len() - 1) {
+        differences.push(values[idx + 1] - val);
+    }
+    let prediction = *values.first().unwrap() - get_past_prediction(differences);
     return prediction;
 }
 
