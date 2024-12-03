d2822c887c15d9f1c410e68912ac4053c8b3d63a
TeemuJal <temesklk@gmail.com>
2024-12-03 22:49:58 +0200

    2024 day03 part two solution

--- 2024/day03/src/main.rs
+++ 2024/day03/src/main.rs
@@ -6,23 +6,31 @@ fn main() {
     let mut split: Vec<_> = input.split("mul(").collect();
     split.remove(0);
     let mut multiplications_sum = 0;
+    let mut run_mul = true;
     for str in split {
-        let split = str.split_once(',');
-        if let Some((number1, rest)) = split {
-            if number1.len() > 3 {
-                continue;
-            }
-            if let Ok(number1) = number1.parse::<usize>() {
-                let split = rest.split_once(')');
-                if let Some((number2, _)) = split {
-                    if number2.len() > 3 {
-                        continue;
-                    }
-                    if let Ok(number2) = number2.parse::<usize>() {
-                        multiplications_sum += number1 * number2;
+        if run_mul {
+            let split = str.split_once(',');
+            if let Some((number1, rest)) = split {
+                if number1.len() > 3 {
+                    continue;
+                }
+                if let Ok(number1) = number1.parse::<usize>() {
+                    let split = rest.split_once(')');
+                    if let Some((number2, _)) = split {
+                        if number2.len() > 3 {
+                            continue;
+                        }
+                        if let Ok(number2) = number2.parse::<usize>() {
+                            multiplications_sum += number1 * number2;
+                        }
                     }
                 }
             }
+        }
+        if str.rfind("don't()").is_some() {
+            run_mul = false;
+        } else if str.rfind("do()").is_some() {
+            run_mul = true;
         }
     }
     println!("Sum of multiplications {multiplications_sum}");
