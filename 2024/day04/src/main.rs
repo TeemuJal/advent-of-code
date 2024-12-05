953e298201dd90602f3c286337998ca56f2fad11
TeemuJal <temesklk@gmail.com>
2024-12-05 21:43:12 +0200

    2024 day04 part two solution

--- 2024/day04/src/main.rs
+++ 2024/day04/src/main.rs
@@ -3,10 +3,40 @@ use std::fs::read_to_string;
 fn main() {
     let input = read_to_string("input2.txt").unwrap();
     let lines: Vec<&str> = input.lines().collect();
+    part_one_xmas_count(&lines);
+    part_two_xmas_count(&lines);
+}
+
+fn part_two_xmas_count(lines: &Vec<&str>) {
+    let line_len = lines[0].len();
+    let mut xmas_count = 0;
+    for line_idx in 1..lines.len() - 1 {
+        let line = lines[line_idx];
+        let prev_line = lines[line_idx - 1];
+        let next_line = lines[line_idx + 1];
+        for hor_idx in 1..line_len - 1 {
+            if line.chars().nth(hor_idx).unwrap() != 'A' {
+                continue;
+            }
+            let top_left = prev_line.chars().nth(hor_idx - 1).unwrap();
+            let top_right = prev_line.chars().nth(hor_idx + 1).unwrap();
+            let bot_left = next_line.chars().nth(hor_idx - 1).unwrap();
+            let bot_right = next_line.chars().nth(hor_idx + 1).unwrap();
+            let diagonal_1_matches = (top_left == 'M' && bot_right == 'S') || (top_left == 'S' && bot_right == 'M');
+            let diagonal_2_matches = (top_right == 'M' && bot_left == 'S') || (top_right == 'S' && bot_left == 'M');
+            if diagonal_1_matches && diagonal_2_matches {
+                xmas_count += 1;
+            }
+        }
+    }
+    println!("X-MAS appears {xmas_count} times");
+}
+
+fn part_one_xmas_count(lines: &Vec<&str>) {
     let line_len = lines[0].len();
     let mut search_strings: Vec<String> = vec![];
     // Horizontals
-    for line in input.lines() {
+    for line in lines {
         search_strings.push(line.to_string());
         search_strings.push(line.chars().rev().collect());
     }
